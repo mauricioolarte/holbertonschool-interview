b5268cec548fb36c2400c62c5de9c478cbb68512
mauricioolarte <reneolarte@yahoo.com>
2020-12-11 00:51:20 -0500

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,7 +2,28 @@
 
 def canUnlockAll(boxes):
   boxLen = len(boxes)
-  if boxLen == 0 or len(boxes[0]):
+  keys = []
+  boxesnumbers = []
+  for i in range(1, boxLen, 1):
+    boxesnumbers.append(i)
+  if boxLen == 0 or len(boxes[0]) == 0:
     return False
+  for i in range(len(boxes[0])):
+    keys.append(boxes[0][i])
+  counter = 0
+  x = 1
+  while(counter < boxLen):
+    for k in range(1, boxLen, 1):
+      if k in keys:
+        for m in range(len(boxes[k])):
+          keys.append(boxes[k][m])
+    counter += 1
+    k = 1
 
-  print(boxes)
+  for j in boxesnumbers:
+    if j not in keys:
+      return False
+  return True
+  
+  
+  print(keys)
