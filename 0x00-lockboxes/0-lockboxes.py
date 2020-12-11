41c95d79960d428d4bc6dfc2a90a0225764df6eb
mauricioolarte <reneolarte@yahoo.com>
2020-12-11 01:29:12 -0500

    0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -18,8 +18,6 @@ def canUnlockAll(boxes):
             if k in keys:
                 for m in range(len(boxes[k])):
                     keys.append(boxes[k][m])
-        if keys == boxesnumbers:
-            return True
         counter += 1
         k = 1
 
