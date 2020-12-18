e1539df61767109a5b187971a52e3733413329f4
mauricioolarte <reneolarte@yahoo.com>
2020-12-17 23:54:19 -0500

    0-insert_number.c

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -3,44 +3,50 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-insert_node - insert a new node
-@head: head linked list
-@number: number to insert
-Return: null or new node
+*Insert_node - insert a new node
+*@head: head linked list
+*@number: number to insert
+*Return: null or new node
 */
 
-listint_t *insert_node(listint_t **head, int number){
-    listint_t *newnode = NULL;
-    listint_t *temporal = NULL;
-    if ((*head) == NULL) {
-        return NULL;
-    }
-    temporal = (listint_t *) malloc(sizeof(listint_t));
-    if (temporal == NULL){
-        return NULL;
-    }
-    temporal = (*head);
-    while(temporal) {
-        if (temporal->next && (temporal->next->n >= number)){
-            if (temporal->n <= number){
-                newnode = malloc(sizeof(listint_t));
-                if (newnode == NULL){
-                    return NULL;
-                }
-                newnode->n = number;
-                newnode->next = temporal->next;
-                temporal->next = newnode;
-                return(newnode);
-           }
-       }         
-    
-       if(temporal->next){
-           temporal = temporal->next;
-       } else {
-           return(NULL);
-       }
-    }
-
-  
-    return(NULL);
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newnode = NULL;
+	listint_t *temporal = NULL;
+	
+	if ((*head) == NULL)
+	{
+		return (NULL);
+	}
+	temporal = (listint_t *) malloc(sizeof(listint_t));
+	if (temporal == NULL)
+	{
+		return (NULL);
+	}
+	temporal = (*head);
+	while(temporal)
+	{
+		if (temporal->next && (temporal->next->n >= number))
+		{
+			if (temporal->n <= number)
+			{
+				newnode = malloc(sizeof(listint_t));
+				if (newnode == NULL){
+				return (NULL);
+			}
+			newnode->n = number;
+			newnode->next = temporal->next;
+			temporal->next = newnode;
+			return(newnode);
+		}         
+		if(temporal->next)
+		{
+			temporal = temporal->next;
+		}
+		else
+		{
+			return(NULL);
+		}
+	}
+	return(NULL);
 }
