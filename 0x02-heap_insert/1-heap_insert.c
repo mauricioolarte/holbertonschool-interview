0968c0ad969266619a6c409e361bf2e357807bd2
mauricioolarte <reneolarte@yahoo.com>
2020-12-26 22:54:21 -0500

    1-heap_insert.c

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+**heap_insert - set binary tree node
+*@root: pointer;
+*@value: value to set in pointer.
+*Return: pointer.
+*/
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	binary_tree_t *newnode = NULL;
+	// binary_tree_t *temp = NULL;
+
+	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	if (*root == NULL)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
+	return (newnode);
+
+
+}
