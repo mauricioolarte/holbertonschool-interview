061cea8d99e5b04bf1d4dc75d43dbe59031df63d
mauricioolarte <reneolarte@yahoo.com>
2020-12-26 22:33:09 -0500

    0-binary_tree_node.c

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -11,32 +11,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = NULL;
 
-	if (parent == NULL)
-	{
-		newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-		if (newnode == NULL)
-			return (NULL);
-		newnode->n = value;
-		return (newnode);
-	}
-	if (parent->left == NULL)
-	{
-		newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-		if (newnode == NULL)
-			return (NULL);
-		parent->left = newnode;
-		newnode->n = value;
-		newnode->parent = parent;
-		return (newnode);
-	} else if (parent->right == NULL)
-	{
-		newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-		if (newnode == NULL)
-			return (NULL);
-		parent->right = newnode;
-		newnode->n = value;
-		newnode->parent = parent;
-		return (newnode);
-	}
-	return (NULL);
+	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return (newnode);
 }
