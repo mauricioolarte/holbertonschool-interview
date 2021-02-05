75b6764678108090ab20f649d67b20ba01812f9a
mauricioolarte <reneolarte@yahoo.com>
2021-02-04 23:34:28 -0500

    0-check_cycle.c

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * check_cycle - list
+ * @list: linked list
+ * Return: int
+ * Description: singly linked list node structure
+ *
+ */
+int check_cycle(listint_t *list)
+{
+
+}
