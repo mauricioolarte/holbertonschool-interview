481ed911aced54e2e8164ee0fcf299077141c4ce
mauricioolarte <reneolarte@yahoo.com>
2021-05-06 21:59:19 -0500

    1-heap_insert.c

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
@@ -11,17 +12,46 @@
 heap_t *heap_insert(heap_t **root, int value)
 {
 	binary_tree_t *newnode = NULL;
-
-	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (newnode == NULL)
-		return (NULL);
+	binary_tree_t *currentnode = NULL;
 
 	if (*root == NULL)
 	{
-		*root = binary_tree_node(NULL, value);
-		return (*root);
+		newnode = binary_tree_node(NULL, value);
+		*root = newnode;
+		printf("hola");
+		return (newnode);
 	}
-	return (newnode);
+	currentnode = *root;
+	while (currentnode != NULL)
+	{
+		if (value > currentnode->n)
+		{
+			newnode = binary_tree_node(currentnode->parent, value);
+			if (currentnode->parent == NULL)
+			{
+				*root = newnode;
+			}
+			currentnode->parent = newnode;
+			newnode->left = currentnode;
+			return (newnode);
+		}
+		else if (currentnode->left == NULL)
+		{
+			newnode = binary_tree_node(currentnode->left, value);
+			currentnode->left = newnode;
+			newnode->parent = currentnode;
+			return newnode;
+		}
+		else if (currentnode->right == NULL)
+		{
+			newnode = binary_tree_node(currentnode->right, value);
+			currentnode->right = newnode;
+			newnode->parent = currentnode;
+			return newnode;
+		}
 
+		currentnode = currentnode->left;
+	}
 
+	return (*root);
 }
