ba6d684ae1e89d3fe72b427e7fb12af760b29938
mauricioolarte <reneolarte@yahoo.com>
2021-05-13 17:50:02 -0500

    0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,14 +6,17 @@ def minOperations(n):
     ''' calcula el numero minimo de operaciones para una impresion'''
     answer = 0
 
-    if n == 0:
-        return answer
-    if n == 1:
-        return answer
+    if type(n) == type(2):
+        if n == 0:
+            return answer
+        if n == 1:
+            return answer
 
-    for number in range(2, n + 1, 1):
-        while n % number == 0 and n / number >= 1:
-            n = n / number
-            answer = answer + number
+        for number in range(2, n + 1, 1):
+            while n % number == 0 and n / number >= 1:
+                n = n / number
+                answer = answer + number
 
-    return int(answer)
+        return int(answer)
+    else:
+        return 0
