359769d6db6fb70df0287c8331095b49e4632ef6
mauricioolarte <reneolarte@yahoo.com>
2021-05-26 22:59:44 -0500

    add files

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * lenght_list - prints all elements of a listint_t list
+ * @head: pointer to head of list
+ * Return: number of nodes
+ */
+int lenght_list(listint_t *head)
+{
+	int counter = 0;
+
+	listint_t *node = NULL;
+
+	node = malloc(sizeof(listint_t));
+
+	node = head;
+	if (node == NULL)
+		return (0);
+
+	while (node != NULL)
+	{
+		counter += 1;
+		node = node->next;
+	}
+	return (counter);
+}
+
+/**
+ * is_palindrome - prints all elements of a listint_t list
+ * @head: pointer to head of list
+ * Return: number of nodes
+ */
+int is_palindrome(listint_t **head)
+{
+	int n = 0, counter = 0, i = 0;
+	int *arr = NULL;
+	listint_t *temp = NULL;
+
+	n = lenght_list(*head);
+	if (n == 0 || n == 1)
+		return (0);
+
+	arr = malloc(sizeof(int) * n);
+	if (arr == NULL)
+		return (0);
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (0);
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		arr[counter] = temp->n;
+		counter += 1;
+		temp = temp->next;
+	}
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		if (arr[i] != arr[n - 1 - i])
+			return (0);
+	}
+
+	return (1);
+}
---
+++ 0x05-linked_list_palindrome/README.md
@@ -0,0 +1,3 @@
+Prototype: int is_palindrome(listint_t **head);
+Return: 0 if it is not a palindrome, 1 if it is a palindrome
+An empty list is considered a palindrome
