2fd5eb6d0ef395afb1dfb79002c94caa5e77677f
mauricioolarte <reneolarte@yahoo.com>
2021-06-06 08:57:35 -0500

    0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,11 +6,14 @@ from datetime import datetime
 
 
 def isIp(ip):
-    values = ip.split(".")
-    for value in values:
-        if int(value) > 255 or int(value) < 0:
-            return 1
-    return 0
+    try:
+        values = ip.split(".")
+        for value in values:
+            if int(value) > 255 or int(value) < 0:
+                return 1
+        return 0
+    except ValueError:
+        return 1
 
 
 def isDate(date):
@@ -30,16 +33,19 @@ def isString(string):
 
 
 def isStatus(status):
-    if status in [200, 301, 400, 401, 403, 404, 405, 500]:
+    if status in ['200', '301', '400', '401', '403', '404', '405', '500']:
         return 0
     else:
         return 1
 
 
 def isFileSize(size):
-    if int(size) >= 0 and int(size) < 1025:
-        return 0
-    else:
+    try:
+        if int(size) >= 0 and int(size) < 1025:
+            return 0
+        else:
+            return 1
+    except ValueError:
         return 1
 
 
@@ -57,7 +63,7 @@ def printAnswer(signal, frame):
     for value, key in statuss.items():
         if key > 0:
             print(("{}: {}").format(value, key))
-    exit(0)
+    sys.exit(0)
 
 
 count = 0
@@ -76,22 +82,29 @@ statuss = {
 signal.signal(signal.SIGINT, printAnswer)
 
 for line in sys.stdin:
-
     count = count + 1
     params = line.split(" - ")
     checkIp = isIp(params[0])
-    checkDate = isDate(params[1][1:26])
-    # print(params[1][1:26])
-    checkString = isString(params[1][30:57])
-    # print(params[1][30:56])
-    checkStatus = isStatus(params[1][58:61])
-    # print(params[1][58:61])
-    checkFileSize = isFileSize(params[1][62:-1])
-    # print(params[1][62:-1])
-    statuss = countStatus(params[1][58:61], statuss)
-    fileSize = fileSize + int(params[1][62:-1])
-    # print(fileSize)
+    if checkIp == 0 and len(params) > 1:
+        checkDate = isDate(params[1][1:26])
+        checkString = isString(params[1][29:57])
+        checkStatus = isStatus(params[1][58:61])
+        # print(params[1][58:61])
+        checkFileSize = isFileSize(params[1][62:-1])
+        # print(params[1][62:-1])
+        statuss = countStatus(params[1][58:61], statuss)
+        try:
+            fileSize = fileSize + int(params[1][62:-1])
+        except ValueError:
+            fileSize = fileSize + 0
+        # print(fileSize)
+
+    if checkIp == 1 or checkDate == 1 or checkStatus == 1:
+        continue
+    if checkFileSize == 1:
+        continue
     # print(line.split(" - "))
+    # print(count)
     if count == 10:
         print(("File size: {}").format(fileSize))
         for value, key in statuss.items():
