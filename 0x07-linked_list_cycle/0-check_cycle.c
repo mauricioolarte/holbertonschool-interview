df1e8edb4def5a036cb4e143d389b85c87704ad4
mauricioolarte <reneolarte@yahoo.com>
2021-06-13 22:19:38 -0500

    0-check_cycle.c

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -12,28 +12,8 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = NULL;
-	listint_t *fast = NULL;
-
 	if (list == NULL)
 		return (0);
-
-	fast = list;
-	slow = list;
-
-	while (slow)
-	{
-		while (fast)
-		{
-			fast = fast->next;
-			if (fast == slow)
-			{
-				return (1);
-			}
-		}
-		fast = slow->next;
-		slow = slow->next;
-	}
 
 	return (0);
 }
