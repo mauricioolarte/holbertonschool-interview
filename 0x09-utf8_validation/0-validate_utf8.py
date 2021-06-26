8936e4cdba19632a5f81ac66a0ea507f73f0e37a
mauricioolarte <reneolarte@yahoo.com>
2021-06-26 11:21:07 -0500

    fix

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
-''' check if array is a utf endcoded'''
+"""
+Test UTF-8
+"""
 
 
 def validUTF8(data):
-    ''' check array '''
-    for bite in data:
-        if bite < 0 or bite > 255:
-            return False
+    """
+    validate if data array is UTF format
+    """
+    validator = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+    for num in data:
+        mask = 1 << 7
+        if validator == 0:
+            while mask & num:
+                validator += 1
+                mask = mask >> 1
+            if validator == 0:
+                continue
+            if validator == 1 or validator > 4:
+                return False
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+        validator -= 1
+    return validator == 0
+
     return True
