1187e854718937d608915bb7a88d599c6bc0e429
mauricioolarte <reneolarte@yahoo.com>
2021-07-22 20:41:38 -0500

    0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -8,16 +8,16 @@ import sys
 global N
 
 if len(sys.argv) > 2:
-    print("Usage: nqueens N\n")
+    print("Usage: nqueens N")
     sys.exit(1)
 
 try:
     N = int(sys.argv[1])
     if N < 4:
-        print("N must be at least 4\n")
+        print("N must be at least 4")
         sys.exit(1)
 except ValueError:
-    print("N must be a number\n")
+    print("N must be a number")
     sys.exit(1)
 
 
